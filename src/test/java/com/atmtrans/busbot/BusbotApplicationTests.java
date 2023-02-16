2d37db6168dabbf92e055f2ab2bdb174788388d8
Artem Marchenko <art.marchenko@gmail.com>
2023-02-16 19:14:40 +0200

    init commit

---
+++ src/test/java/com/atmtrans/busbot/BusbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmtrans.busbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
