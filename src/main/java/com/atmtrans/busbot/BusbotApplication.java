147046eb899ff94b803979003c0b22ecc7c020d9
vladyslavyarets <zzzlpzzz0@gmail.com>
2023-03-08 23:42:18 +0200

    WIP: test coverage test

--- src/main/java/com/atmtrans/busbot/BusbotApplication.java
+++ src/main/java/com/atmtrans/busbot/BusbotApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BusbotApplication {
 
     public static void main(String[] args) {
+        System.out.println("hi");
         SpringApplication.run(BusbotApplication.class, args);
     }
 }
