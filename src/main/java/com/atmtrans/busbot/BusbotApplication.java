69d05bcae37a8ac23e72eb74e3bc8aec138ecf4f
vladyslavyarets <zzzlpzzz0@gmail.com>
2023-03-09 21:01:12 +0200

    Remove WIP stub code

--- src/main/java/com/atmtrans/busbot/BusbotApplication.java
+++ src/main/java/com/atmtrans/busbot/BusbotApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BusbotApplication {
 
     public static void main(String[] args) {
-        System.out.println("hi");
         SpringApplication.run(BusbotApplication.class, args);
     }
 }
